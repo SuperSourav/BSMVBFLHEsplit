d3ee3e8d914a0cfc1991193a1dbed80d6a07354e
Sourav Sen <sourav.sen@cern.ch>
2018-11-30 22:11:21 +0100

    validation script added with the flexibility to change dsids and channel

--- validation.sh
+++ validation.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+DSID=363769;
+channel="vbf_eta700_140_zz_llll_13TeV";
 rm splitres.txt orires.txt;
-grep "^  9000006" /afs/cern.ch/user/a/akotwal/public/group.phys-gener.MG5_aMCatNLO201.363762.vbf_eta400_80_zz_llnunu_13TeV.TXT.mc15_v1._00001.events >> orires.txt;
-for i in `seq 55`;
+grep "^  9000006" /afs/cern.ch/user/a/akotwal/public/group.phys-gener.MG5_aMCatNLO201.${DSID}.${channel}.TXT.mc15_v1._00001.events >> orires.txt;
+for i in `seq 19`;
+	#`seq 55`;
 do
   if [ $i -lt 10 ]; then 
     n=0$i
@@ -10,7 +13,7 @@ do
     n=$i
   fi
   echo ${n}
-  grep "^  9000006" group.phys-gener.MG5_aMCatNLO201.363762.vbf_eta400_80_zz_llnunu_13TeV.TXT.mc15_v1._000${n}.events | wc -l
-  grep "^  9000006" group.phys-gener.MG5_aMCatNLO201.363762.vbf_eta400_80_zz_llnunu_13TeV.TXT.mc15_v1._000${n}.events >> splitres.txt
+  grep "^  9000006" group.phys-gener.MG5_aMCatNLO201.${DSID}.${channel}.TXT.mc15_v1._000${n}.events | wc -l
+  grep "^  9000006" group.phys-gener.MG5_aMCatNLO201.${DSID}.${channel}.TXT.mc15_v1._000${n}.events >> splitres.txt
 
 done
